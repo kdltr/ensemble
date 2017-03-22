98b76ab326bb36b27396888aa6f5439e145e9629
Kooda <kooda@upyum.com>
2017-03-22 15:03:36 +0100

    Add a config file to ease prototyping.

--- matrix.scm
+++ matrix.scm
@@ -13,3 +13,15 @@
 (define logout
   (let ((old-logout logout))
     (lambda () (old-logout '()))))
+
+;; Config file
+;; ===========
+
+(define (save-config)
+  (with-output-to-file "config.scm"
+    (lambda ()
+      (for-each write `((init! ,(uri->string (server-uri)))
+                        (access-token ,(access-token)))))))
+
+(when (file-exists? "config.scm")
+  (for-each eval (read-file "config.scm")))
