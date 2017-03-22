22d1828a6de6e11fab7c59861ec8589b5b849136
Kooda <kooda@upyum.com>
2017-03-22 01:19:07 +0100

    password-login and logout procedure of the high level API

--- matrix.scm
+++ matrix.scm
@@ -1,6 +1,15 @@
 (load "low-level")
 
+(use clojurian-syntax)
+
 (define (password-login user password)
-  (login `((type . "m.login.password")
-           (user . ,user)
-           (password . ,password))))
+  (->> (login `((type . "m.login.password")
+                (user . ,user)
+                (password . ,password)))
+       (alist-ref 'access_token)
+       (access-token)))
+
+;; TODO do that properly when low-level is a module
+(define logout
+  (let ((old-logout logout))
+    (lambda () (old-logout '()))))
