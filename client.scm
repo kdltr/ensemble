62d480f041bd1f499f33f4554d60399ef6b77209
Kooda <kooda@upyum.com>
2017-03-23 15:00:43 +0100

    Add event context storage and timeline advancing.

--- client.scm
+++ client.scm
@@ -16,3 +16,75 @@
 
 (when (file-exists? "config.scm")
   (for-each eval (read-file "config.scm")))
+
+
+;; Events handling
+;; ===============
+
+(define context-updaters
+  `((m.room.create . ,(lambda (ctx evt)
+                        (mupdate '(creator)
+                                (mref '(content creator) evt)
+                                ctx)))
+    (m.room.member . ,(lambda (ctx evt)
+                        (let ((membership (string->symbol (mref '(content membership) evt)))
+                              (who (mref '(state_key) evt))
+                              (display-name (mref '(content displayname) evt))
+                              (avatar-url (mref '(content avatar_url) evt)))
+                          (as-> ctx ctx
+                                (case membership
+                                  ((invite)
+                                   (mupdate '(invited) (lset-adjoin string=? (or (mref '(invited) ctx) '()) who) ctx))
+                                  ((join)
+                                   (mupdate '(members) (lset-adjoin string=? (or (mref '(members) ctx) '()) who) ctx))
+                                  ((leave)
+                                   (mupdate '(members) (delete who (or (mref '(members) ctx) '())) ctx)))
+                                (if display-name
+                                    (if (eq? display-name 'null)
+                                        (mdelete `(member-names ,who) ctx)
+                                        (mupdate `(member-names ,who) display-name ctx))
+                                    ctx)
+                                (if avatar-url
+                                    (if (eq? avatar-url 'null)
+                                        (mdelete `(member-avatars ,who) ctx)
+                                        (mupdate `(member-avatars ,who) avatar-url ctx))
+                                    ctx)))))
+    (m.room.name . ,(lambda (ctx evt)
+                      (let ((name (mref '(content name) evt)))
+                        (if (member name '(#f "" null))
+                            (mdelete '(name) ctx)
+                            (mupdate '(name) name ctx)))))
+    (m.room.topic . ,(lambda (ctx evt)
+                       (let ((topic (mref '(content topic) evt)))
+                         (if (member topic '(#f "" null))
+                             (mdelete '(topic) ctx)
+                             (mupdate '(topic) topic ctx)))))
+    (m.room.avatar . ,(lambda (ctx evt)
+                        (let ((url (mref '(content url) evt)))
+                          (mupdate '(avatar) url ctx))))
+    ))
+
+(define (update-context ctx evt)
+  (let* ((evt-type (string->symbol (mref '(type) evt)))
+         (updater (alist-ref evt-type context-updaters)))
+    (if updater
+        (updater ctx evt)
+        (begin 
+          (warning "Event of unknown type" evt-type)
+          ctx))))
+
+(define (initial-context state)
+  (vector-fold (lambda (i ctx evt) (update-context ctx evt)) '() state))
+
+(define (advance-timeline timeline ctx evts)
+  (let loop ((ctx ctx)
+             (i 0)
+             (timeline timeline))
+    (if (= i (vector-length evts))
+        timeline
+        (let* ((evt (vector-ref evts i))
+               (new-ctx (update-context ctx evt)))
+          (loop new-ctx
+                (add1 i)
+                (cons (mupdate '(_context) new-ctx evt)
+                      timeline))))))
--- matrix.scm
+++ matrix.scm
@@ -5,12 +5,25 @@
 ;; Helper procedures
 ;; =================
 
-(define (ref keys alist)
+(define (mref keys alist)
   (if (null? keys)
       alist
       (and-let* ((o (alist-ref (car keys) alist)))
-           (ref (cdr keys) o))))
+           (mref (cdr keys) o))))
 
+(define (mupdate keys val alist)
+  (if (null? (cdr keys))
+      (alist-update (car keys) val alist)
+      (alist-update (car keys)
+                    (mupdate (cdr keys) val (or (alist-ref (car keys) alist) '()))
+                    alist)))
+
+(define (mdelete keys alist)
+  (if (null? (cdr keys))
+      (alist-delete (car keys) alist)
+      (alist-update (car keys)
+                    (mdelete (cdr keys) (or (alist-ref (car keys) alist) '()))
+                    alist)))
 
 ;; High level API
 ;; ==============
