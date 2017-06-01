07560f42619fc827fc51572462dffee3d5196a69
Kooda <kooda@upyum.com>
2017-06-01 18:54:43 +0200

    Extract character width info from the UCD and put that in a tree.

--- extract.scm
+++ extract.scm
@@ -1,25 +1,78 @@
 (import scheme)
+(use srfi-13 srfi-1)
 
-(define vec
-  (make-u8vector (add1 (quotient #xFFFF 8)) 0))
+(define port (open-input-file "EastAsianWidth.txt"))
 
-(define (bit-set! vec index bit)
-  (u8vector-set! vec (quotient index 8)
-                 (bitwise-ior (* (if bit 1 0) (expt 2 (modulo index 8)))
-                              (u8vector-ref vec (quotient index 8)))))
+(define (translate-width sym)
+  (case sym
+    ((F) 'full-width)
+    ((H) 'half-width)
+    ((W) 'wide)
+    ((Na) 'narrow)
+    ((A) 'ambiguous)
+    ((N) 'neutral)
+    (else (error "unknown width" sym))))
 
-(define (bit-ref vec index)
-  (< 0 (bitwise-and (u8vector-ref vec (quotient index 8))
-                    (expt 2 (modulo index 8)))))
+(define (extract)
+  (let ((line (read-line port)))
+    (if (eof-object? line)
+        '()
+        (extract-line line))))
 
-(define (extract line)
-  (let* ((split (string-split line ":"))
-         (charnum (string->number (car split) 16))
-         (charwidth (/ (string-length (cadr split)) 32)))
-    (assert (<= 1 charwidth 2))
-    (bit-set! vec charnum (= charwidth 2))))
+(define (extract-line line)
+  (let* ((uncomment (string-split line "#" #t))
+         (data (string-split (string-trim-both (car uncomment)) ";")))
+    (if (null? data)
+        (extract)
+        (let ((range (map (cut string->number <> 16) (string-split (car data) ".")))
+              (width (translate-width (string->symbol (cadr data)))))
+          (if (= 1 (length range))
+              (cons (list (car range) (car range) width) (extract))
+              (cons (list (car range) (cadr range) width) (extract)))))))
 
-(define truc
-(call-with-input-file "unifont.hex"
-  (lambda (p)
-    (port-for-each extract (lambda () (read-line p))))))
+(define l (extract))
+
+;; using vectors because they have a litteral representation
+
+;; node vector:
+;; 0 minimum
+;; 1 left
+;; 2 maximum
+;; 3 right
+;; 4 value
+
+(define (make-tree l)
+  (if (null? l) '()
+  (let*-values (((len) (length l))
+                ((left right) (split-at l (quotient len 2)))
+                ((data) (car right)))
+    (vector (car data)
+            (make-tree left)
+            (cadr data)
+            (make-tree (cdr right))
+            (caddr data)))))
+
+(define (search n t)
+  (let ((minimum (vector-ref t 0))
+        (maximum (vector-ref t 2)))
+    (cond ((>= maximum n minimum)
+           (vector-ref t 4))
+          ((< n minimum)
+           (search n (vector-ref t 1)))
+          ((> n maximum)
+           (search n (vector-ref t 3))))))
+
+(define t (make-tree l))
+
+(define (check node)
+  (let ((minimum (car node))
+        (maximum (cadr node))
+        (value (caddr node)))
+    (every (lambda (n) (eq? (search n t) value))
+           (iota (add1 (- maximum minimum)) minimum))))
+
+(unless (every check l)
+  (error "The tree doesn’t contain the right stuff"))
+
+(with-output-to-file "ucd.dat.scm"
+  (lambda () (write t)))
