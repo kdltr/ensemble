8e0016ad226698c210a024f8f17356f7604114e9
Kooda <kooda@upyum.com>
2017-07-27 15:51:32 +0200

    Remove the TUI and tui-lib, I’ll be trying with ncurses as of now.

--- tui/init.scm
+++
@@ -1,9 +0,0 @@
-;; WHOLE TERMINAL
-
-(define-values (rows columns) (terminal-size termout))
-(define buffer (make-grid columns rows))
-
-(load "tui/input")
-(load "tui/title")
-(load "tui/status")
-(load "tui/central-frame")
