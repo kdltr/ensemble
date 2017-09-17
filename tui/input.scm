ccc9144865ba31503542421c593e5b3084316b67
Kooda <kooda@upyum.com>
2017-09-17 17:11:47 +0200

    Add input line editing support.

--- tui/input.scm
+++ tui/input.scm
@@ -1,6 +1,3 @@
-(define input-string "") ;; buffer for user input
-
-
 ;; Entry point procedure
 (define (handle-input in)
   (cond ((or (number? in) (control-char? in))
@@ -15,16 +12,61 @@
 
 (define (refresh-inputwin)
   (wclear inputwin)
-  (wprintw inputwin
-           (if (>= (string-length input-string) cols)
-               (substring input-string (- (string-length input-string) cols -1))
-               input-string))
+  (wprintw inputwin "~A" (buffer-window))
+  (wmove inputwin 0 (cursor-position))
   (wrefresh inputwin))
 
 
-(define (handle-char c)
-  (set! input-string (string-append input-string (string c))))
 
+;; Buffer for user input
+;; =====================
+
+(define input-string "")
+(define cursor-pos 0)
+(define extent-start 0)
+
+(define (move-cursor #!optional (n 1))
+  (let ((mvmt (case n
+                ((left) 0)
+                ((right) (string-length input-string))
+                (else (max 0 (min (+ cursor-pos n)
+                                  (string-length input-string)))))))
+    (set! cursor-pos mvmt)
+    (move-extent cursor-pos)))
+
+(define (move-extent pos)
+  (let ((ok-left (<= extent-start pos))
+        (ok-right (> (+ extent-start (sub1 cols)) pos)))
+    (cond ((not ok-left)
+           (set! extent-start (max (- pos cols) 0)))
+          ((not ok-right)
+           (set! extent-start
+             (- pos (sub1 cols)))))))
+
+(define (buffer-window)
+  (substring input-string
+             extent-start
+             (min (+ extent-start (sub1 cols))
+                  (string-length input-string))))
+
+(define (cursor-position)
+  (- cursor-pos extent-start))
+
+(define (buffer-insert! c)
+  (set! input-string
+    (string-append (substring input-string 0 cursor-pos)
+                   (string c)
+                   (substring input-string cursor-pos))))
+
+(define (buffer-kill!)
+  (set! input-string
+    (substring input-string 0 cursor-pos)))
+
+(define (handle-char c)
+  (buffer-insert! c)
+  (move-cursor))
+
+(trace substring cursor-position)
 
 
 ;; Key bindings
@@ -38,18 +80,35 @@
 
 (define-key KEY_BACKSPACE
   (unless (string=? input-string "")
-    (set! input-string (substring input-string 0 (sub1 (string-length input-string))))))
+    (set! input-string (substring input-string 0 (sub1 (string-length input-string))))
+    (move-cursor -1)))
 
 (define-key KEY_RESIZE
   (set!-values (rows cols) (getmaxyx (stdscr)))
   (for-each redrawwin (list statuswin inputwin (room-window (alist-ref (current-room) *rooms*)))))
+
+(define-key KEY_LEFT
+  (move-cursor -1))
+
+(define-key KEY_RIGHT
+  (move-cursor 1))
+
+(define-key #\x01 ;; C-a
+  (move-cursor 'left))
+
+(define-key #\x05 ;; C-e
+  (move-cursor 'right))
+
+(define-key #\x0B ;; C-k
+  (buffer-kill!))
 
 (define-key #\newline
   (unless (equal? input-string "")
     (if (char=? (string-ref input-string 0) #\/)
         (handle-command input-string)
         (message:text (current-room) input-string))
-    (set! input-string "")))
+    (set! input-string "")
+    (move-cursor 'left)))
 
 
 
