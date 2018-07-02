b79338e37ba4069d281394042027081e4e23bc50
Kooda <kooda@upyum.com>
2018-07-02 17:06:34 +0200

    Update testbench.scm to use the new modules

--- testbench.scm
+++ testbench.scm
@@ -1,22 +1,5 @@
-(import
-  (except scheme
-          string-length string-ref string-set! make-string string substring
-          string->list list->string string-fill! write-char read-char display)
-  (except chicken
-          reverse-list->string print print*)
-  (except data-structures
-          ->string conc string-chop string-split string-translate
-          substring=? substring-ci=? substring-index substring-index-ci)
-  (except extras
-          read-string write-string read-token)
-  ports files srfi-4)
+(load "ensemble.scm")
+(import frontend backend)
 
-(use utf8 utf8-srfi-13 utf8-srfi-14 unicode-char-sets
-     vector-lib clojurian-syntax uri-common openssl
-     ncurses gochan miscmacros srfi-1 posix irregex
-     srfi-18 intarweb (except medea read-json) cjson
-     rest-bind uri-common (prefix http-client http:)
-     sql-de-lite lru-cache ioctl trace)
-
-(load "db.scm")
-(load "client.scm")
+(define first-batch (sync))
+(define next (handle-sync first-batch))
