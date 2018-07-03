58459a24a3e5ca9a3ad6e7163ba55a9134bf9a7b
Kooda <kooda@upyum.com>
2018-07-03 14:15:38 +0200

    Add the nonblocking-ports module

---
+++ nonblocking-ports.scm
@@ -0,0 +1,45 @@
+(declare (disable-interrupts))
+
+(module nonblocking-ports (open-input-file*/nonblocking
+                           open-output-file*/nonblocking)
+(import scheme chicken foreign ports)
+(use posix srfi-18)
+
+(foreign-declare "#include <errno.h>")
+(define EAGAIN (foreign-value "EAGAIN" integer))
+(define EWOULDBLOCK (foreign-value "EWOULDBLOCK" integer))
+
+(define ((fd-read-char fd))
+  (thread-wait-for-i/o! fd #:input)
+  (let* ((str+len (file-read fd 1)))
+    (if (= (cadr str+len) 0)
+        '#!eof
+        (string-ref (car str+len) 0))))
+
+(define ((input-char-ready? fd))
+  (call-with-values
+    (lambda () (file-select fd #f 0))
+    (lambda (r _) r)))
+
+(define (open-input-file*/nonblocking fd)
+  (make-input-port (fd-read-char fd)
+                   (input-char-ready? fd)
+                   (lambda () (file-close fd))))
+
+
+
+(define ((fd-write-string fd) str)
+  (condition-case (file-write fd str)
+    (exn (exn i/o file) (if (or (= (errno) EAGAIN)
+                                (= (errno) EWOULDBLOCK))
+                            (begin
+                              (thread-wait-for-i/o! fd)
+                              ((fd-write-string fd) str))
+                            (signal exn)))))
+
+(define (open-output-file*/nonblocking fd)
+  (file-control fd fcntl/setfl (bitwise-ior (file-control fd fcntl/getfl)
+                                            open/nonblock))
+  (make-output-port (fd-write-string fd)
+                    (lambda () (file-close fd))))
+)
