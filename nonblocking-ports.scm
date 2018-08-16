882582bf190a6c730fbbc360e051776d5be64889
Kooda <kooda@upyum.com>
2018-08-16 11:49:59 +0200

    Add buffering in nonblocking-ports

--- nonblocking-ports.scm
+++ nonblocking-ports.scm
@@ -5,36 +5,98 @@
 (import scheme chicken foreign ports)
 (use posix srfi-18 srfi-71)
 
-(define ((fd-read-char fd))
-  (thread-wait-for-i/o! fd #:input)
-  (let* ((str+len (file-read fd 1)))
-    (if (= (cadr str+len) 0)
-        '#!eof
-        (string-ref (car str+len) 0))))
+(define BUFSIZE 1024)
 
-(define ((input-char-ready? fd))
-  (let ((r _ (file-select fd #f 0)))
-    r))
+;; INPUT ports
+;; ===========
 
 (define (open-input-file*/nonblocking fd)
-  (make-input-port (fd-read-char fd)
-                   (input-char-ready? fd)
+  (define buf (make-string BUFSIZE #\null))
+  (define len 0)
+  (define pos 0)
+
+  (define (fd-read-char)
+    (if (< pos len)
+        (let ((c (string-ref buf pos)))
+          (set! pos (add1 pos))
+          c)
+        (begin
+          (thread-wait-for-i/o! fd #:input)
+          (let* ((str+len (file-read fd BUFSIZE buf))
+                 (read-len (cadr str+len)))
+            (if (zero? read-len)
+                '#!eof
+                (begin
+                  (set! pos 0)
+                  (set! len read-len)
+                  (fd-read-char)))))))
+
+  (define (input-char-ready?)
+    (or (< pos len)
+        (let ((r _ (file-select fd #f 0)))
+          r)))
+
+  (make-input-port fd-read-char
+                   input-char-ready?
                    (lambda () (file-close fd))))
 
-
-
-(define ((fd-write-string fd) str)
-  (condition-case (file-write fd str)
-    (exn (exn i/o file) (if (or (= (errno) errno/again)
-                                (= (errno) errno/wouldblock))
-                            (begin
-                              (thread-wait-for-i/o! fd)
-                              ((fd-write-string fd) str))
-                            (signal exn)))))
+;; OUTPUT ports
+;; ============
 
 (define (open-output-file*/nonblocking fd)
+  (define buf (make-string BUFSIZE #\null))
+  (define pos 0)
+
+  (define (write-buf str)
+    (copy-string! buf pos str)
+    (set! pos (+ pos (string-length str))))
+
+  (define (flush-buf)
+    (write-all-data fd buf pos)
+    (set! pos 0))
+
+  (define (fd-write-string str)
+    (let ((str-len (string-length str))
+          (buf-free (- BUFSIZE pos)))
+      (cond ((> str-len BUFSIZE)
+             (flush-buf)
+             (write-all-data fd str str-len))
+            ((<= str-len buf-free)
+             (write-buf str))
+            (else
+              (flush-buf)
+              (write-buf str)))))
+
   (file-control fd fcntl/setfl (bitwise-ior (file-control fd fcntl/getfl)
                                             open/nonblock))
-  (make-output-port (fd-write-string fd)
-                    (lambda () (file-close fd))))
-)
+  (make-output-port fd-write-string
+                    (lambda () (file-close fd))
+                    flush-buf))
+
+(define (with-nonblocking-write fd thunk)
+  (condition-case
+    (thunk)
+    (exn (exn i/o file)
+         (if (or (= (errno) errno/again)
+                 (= (errno) errno/wouldblock))
+             (begin
+               (thread-wait-for-i/o! fd)
+               (with-nonblocking-write fd thunk))))))
+
+(define (copy-string! target tpos source)
+  (let lp ((spos 0)
+           (tpos tpos))
+    (unless (= spos (string-length source))
+      (string-set! target tpos (string-ref source spos))
+      (lp (add1 spos) (add1 tpos)))))
+
+(define (write-all-data fd str len)
+  (with-nonblocking-write fd
+    (lambda ()
+      (let lp ((written-len (file-write fd str len)))
+        (when (< written-len len)
+          (set! str (substring str written-len len))
+          (set! len (- len written-len))
+          (lp (file-write fd str len)))))))
+
+) ; module nonblocking-ports
